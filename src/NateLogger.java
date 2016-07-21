c5aeb2146e63a4c29e3bdf0ed6e1ca59496e5361
Nathaniel Hoefer <nathaniel.d.hoefer@gmail.com>
2016-07-20 22:14:15 -0500

    Begin implementing Logging TODO: Get logger method to work.

---
+++ src/NateLogger.java
@@ -0,0 +1,21 @@
+import java.util.logging.*;
+
+
+public class NateLogger {
+
+	
+	
+	public static void main(String[] args) {
+	
+		//Logger logger = new Logger("TestLogger", "TestBundle");
+		
+		//FileHandler fh = new FileHandler("D:/Google Drive/04._Libraries/04._Computer_Design/Testing/TestLog.txt");
+		
+		System.out.println("This is the first line");
+		//ogger.addHandler(fh);
+		
+		System.out.println("This is the second line");
+		
+	}
+	
+}
